72f56c7b372c8fd1b857cce47e9aee40493e9261
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-08-31 14:39:52 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+// Slide 1 - Programação Imperativa
+
+/*//Exercicios 01 > soma entre inteiros
+int main(void) {
+int a;
+int b;
+  printf("Digite seu primeiro operador: ");
+  scanf("%i", &a);
+  printf("Digite seu segundo operador: ");
+  scanf("%i", &b);
+  printf("O valor de %d + %d = %d", a, b, a+b);
+}*/
+
+/*//Exercicios 02 > subtração entres n reais
+int main() {
+  float a;
+  float b;
+  printf("Digite seu primeiro operador: ");
+  scanf("%f", &a);
+  printf("Digite seu segundo operador: ");
+  scanf("%f", &b);
+  printf("O valor de %f - %f = %.1f", a, b, a-b);
+}*/
+
+/*//Exercicios 03 > divisão é resto de numeros int
+int main(void){
+  int a;
+  int b;
+  printf("Digite seu primeiro operador: ");
+  scanf("%d", &a);
+  printf("Digite seu segundo operador: ");
+  scanf("%d", &b);
+  printf("O valor de %d / %d = %d e o seu resto é %d", a, b, a / b, a % b);
+}*/
+
+/*//Exercicios 04 > divisão de n reais
+int main() {
+  float a;
+  float b;
+  printf("Digite seu primeiro operador: ");
+  scanf("%f", &a);
+  printf("Digite seu segundo operador: ");
+  scanf("%f", &b);
+  printf("O valor de %f / %f = %.3f", a, b, a / b);
+  }*/
+
+/*//Exercicios 05 multiplicação de n reais
+int main() {
+  float x;
+  float y;
+  printf("Digite seu primeiro operador: ");
+  scanf("%f", &x);
+  printf("Digite seu segundo operador: ");
+  scanf("%f", &y);
+  printf("O valor de %f x %f = %.1f", x, y, x * y);
+  }*/
+
+/*//Exercicios 06 > tabela ASCII em CHAR(%c), Decimal(%d) e Hexadecimal(%x)
+int main(void){
+  char a;
+  printf("\nDigite caracter: ");
+  scanf("%c", &a);
+  printf("\nCaracter digitado: %c \nCodigo ASCII: em decimal %d e hexadecimal
+%x", a, a, a); return 0;
+}*/
